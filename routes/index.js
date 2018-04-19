d7505dc9711419eedf94de332c7c1eae58824639
wjzw136 <wjzw136@qq.com>
2018-04-19 23:18:39 +0800

    の33
    333

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,17 @@ router.get('/2', function(req, res, next) {
 router.get('/main', function(req, res, next) {
   res.render('main', { title: 'sdsd' });
 });
+router.get('/news',function(req,res,next){
+  res.render('main',{title:'new'})
+})
+router.get('/cp',function(req,res,next){
+  res.render('main',{title:'new'})
+})
+router.get('/fa',function(req,res,next){
+  res.render('main',{title:'new'})
+})
+router.get('/lx',function(req,res,next){
+  res.render('main',{title:'new'})
+})
+
 module.exports = router;
