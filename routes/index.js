6c85f3f7e0fd6a711a6ac6db5b3afc7419a0a9b8
wjzw136 <wjzw136@qq.com>
2018-04-20 17:21:28 +0800

    2222
    2222

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,39 @@
-var express = require('express');
+var express = require("express");
+var fs = require("fs");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/1', function(req, res, next) {
-  res.render('index1', { title: 'sdsd' });
+router.get("/1", function(req, res, next) {
+  res.render("index1", { title: "sdsd" });
 });
-router.get('/2', function(req, res, next) {
-  res.render('2', { title: 'sdsd' });
+router.get("/2", function(req, res, next) {
+  res.render("2", { title: "sdsd" });
 });
-router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'sdsd' });
+router.get("/main", function(req, res, next) {
+  res.render("main", { title: "sdsd" });
 });
-router.get('/news',function(req,res,next){
-
-  data
-  res.render('main',{data:data})
-})
-router.get('/cp',function(req,res,next){
-  res.render('main',{title:'new'})
-})
-router.get('/fa',function(req,res,next){
-  res.render('main',{title:'new'})
-})
-router.get('/lx',function(req,res,next){
-  res.render('main',{title:'new'})
-})
+router.get("/news", function(req, res, next) {
+ var data1= fs.readFile("./con.json", 'utf-8',function(err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      //console.log(JSON.parse(data).jianjie);
+      return JSON.parse(data).jianjie;
+    }
+  });
+  res.render("moban", {data:data1});
+});
+router.get("/cp", function(req, res, next) {
+  res.render("main", { title: "new" });
+});
+router.get("/fa", function(req, res, next) {
+  res.render("main", { title: "new" });
+});
+router.get("/lx", function(req, res, next) {
+  res.render("main", { title: "new" });
+});
 
 module.exports = router;
