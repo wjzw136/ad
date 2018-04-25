5276e0395aad6591f515e0088c6af8762a6b154e
wjzw136 <wjzw136@qq.com>
2018-04-25 17:35:31 +0800

    22222
    2222123132

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var fs = require("fs");
+var mysql = require("mysql");
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,6 @@ router.get("/", function(req, res, next) {
   res.render("2", { title: "Express" });
 });
 router.get("/index", function(req, res, next) {
-
   res.render("2", { title: "sdsd" });
 });
 router.get("/main", function(req, res, next) {
@@ -29,20 +29,47 @@ router.get("/cp", function(req, res, next) {
   res.render("cp", { title: "new" });
 });
 router.get("/fa", function(req, res, next) {
-  res.render("xiexie", { main: "还没写，请等待。。。。"  });
+  res.render("xiexie", { main: "还没写，请等待。。。。" });
 });
 router.get("/lx", function(req, res, next) {
-  res.render("xiexie", { main: "还没写，请等待。。。。"  });
+  res.render("xiexie", { main: "还没写，请等待。。。。" });
 });
-
 
 //留言
 router.post("/liuyan", function(req, res, next) {
- // let email=req.body.email;
+  // let email=req.body.email;
   //let dianhua=req.bady.dianhua;
   //let neirong =req.body.neirong;
   //console.log(email+dianhua+neirong)
-  console.log(req.body.email)
+  console.log(req.body.email);
   res.render("xiexie", { main: "谢谢您提出宝贵意见！" });
+});
+
+router.get("/login1", function(req, res, next) {
+  var zhanghao = "ts123"; //req.query.zhanghao;
+  let password = req.query.password;
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "tonesat"
+  });
+  connection.connect();
+  connection.query(
+    "SELECT * FROM yuangong WHERE zhanghao=" + connection.escape(zhanghao),
+    function(err, rows, fields) {
+      if (err) throw err;
+      console.log(rows[0].name);
+      if (rows[0].mima == password) {
+        res.cookie("user", req.query.user);
+        res.cookie("password", req.query.password);
+        res.render("index", {main:"ssss"});
+      } else {
+        res.render("xiexie",{main:"sdsdsd"});
+      }
+    }
+  );
+  connection.end();
+  //console.log(row);
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var mysql= require('mysql');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  res.render("admin/login", { main: "还没写，请等待。。。。"  });
 });
-router.get('/2', function(req, res, next) {
-  res.send('respond             sdsds with a resource');
+
+
+router.get('/1', function(req, res, next) {
+  res.render("admin/1", { main: "还没写，请等待。。。。"  });
 });
+
 
 module.exports = router;
